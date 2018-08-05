c95e6843019250983daab0540cecbe446a70ab22
Meredith Reece <mardebugger@gmail.com>
2018-08-05 01:48:21 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -3,4 +3,8 @@ class Admin::PreferencesController < ApplicationController
   def index
     @preference = Preference.first_or_create(allow_create_songs: true, allow_create_artists: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
+
+  def update
+    @preference = Preference.find(params[:id])
+  end
 end
