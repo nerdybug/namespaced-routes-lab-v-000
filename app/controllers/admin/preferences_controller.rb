1ed0e91813e0436d320097a4c85e08ac421f4473
Meredith Reece <mardebugger@gmail.com>
2018-08-05 01:46:50 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,3 +1,6 @@
 class Admin::PreferencesController < ApplicationController
 
+  def index
+    @preference = Preference.first_or_create(allow_create_songs: true, allow_create_artists: true, song_sort_order: "DESC", artist_sort_order: "DESC")
+  end
 end
